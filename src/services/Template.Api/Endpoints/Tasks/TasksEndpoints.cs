533aa934213d3707a4107c6c5696caecb0ab57e1
vokabud <o.dubakov@gmail.com>
2026-02-25 16:03:42 +0200

    Add CRUD api

--- src/services/Template.Api/Endpoints/Tasks/TasksEndpoints.cs
+++ src/services/Template.Api/Endpoints/Tasks/TasksEndpoints.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Template.Api.Domain;
 using Template.Api.Persistence;
 
 namespace Template.Api.Endpoints.Tasks;
@@ -7,15 +8,73 @@ public static class TasksEndpoints
 {
     public static void MapTasksEndpoints(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/tasks", GetDevices);
+        app.MapGet("/tasks", GetTasks);
+        app.MapGet("/tasks/{id:guid}", GetTaskById);
+        app.MapPost("/tasks", CreateTask);
+        app.MapPut("/tasks/{id:guid}", UpdateTask);
+        app.MapDelete("/tasks/{id:guid}", DeleteTask);
     }
 
-    static async Task<IResult> GetDevices(IApplicationDbContext dbContext)
+    static async Task<IResult> GetTasks(IApplicationDbContext dbContext)
     {
         var tasks = await dbContext
             .Tasks
             .ToListAsync();
 
         return Results.Ok(tasks);
+    }
+
+    static async Task<IResult> GetTaskById(Guid id, IApplicationDbContext dbContext)
+    {
+        var task = await dbContext.Tasks.FindAsync(id);
+
+        if (task is null)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.Ok(task);
+    }
+
+    static async Task<IResult> CreateTask(TaskEntity task, IApplicationDbContext dbContext)
+    {
+        task.Id = Guid.NewGuid();
+
+        dbContext.Tasks.Add(task);
+        await dbContext.SaveChangesAsync();
+
+        return Results.Created($"/tasks/{task.Id}", task);
+    }
+
+    static async Task<IResult> UpdateTask(Guid id, TaskEntity input, IApplicationDbContext dbContext)
+    {
+        var task = await dbContext.Tasks.FindAsync(id);
+
+        if (task is null)
+        {
+            return Results.NotFound();
+        }
+
+        task.Name = input.Name;
+        task.Description = input.Description;
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok(task);
+    }
+
+    static async Task<IResult> DeleteTask(Guid id, IApplicationDbContext dbContext)
+    {
+        var task = await dbContext.Tasks.FindAsync(id);
+
+        if (task is null)
+        {
+            return Results.NotFound();
+        }
+
+        dbContext.Tasks.Remove(task);
+        await dbContext.SaveChangesAsync();
+
+        return Results.NoContent();
     }
 }
