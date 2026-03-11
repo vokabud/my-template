9323aa9f3eae369c7dae21c0ce17cc595228eedf
vokabud <o.dubakov@gmail.com>
2026-03-11 11:02:38 +0200

    Add podman config for aspire

--- src/services/Template.AppHost/Program.cs
+++ src/services/Template.AppHost/Program.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("postgres");
-var database = postgres.AddDatabase("templatedb");
+var database = postgres.AddDatabase("DefaultConnection");
 
 builder.AddProject<Projects.Template_Api>("template-api")
     .WithReference(database);
