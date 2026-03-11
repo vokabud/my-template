c88387ed123977962a63ac8123c9676682d090f7
vokabud <o.dubakov@gmail.com>
2026-03-11 15:40:17 +0200

    Add podman config

--- src/services/Template.AppHost/Program.cs
+++ src/services/Template.AppHost/Program.cs
@@ -3,7 +3,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 var postgres = builder.AddPostgres("postgres");
 var database = postgres.AddDatabase("DefaultConnection");
 
-builder.AddProject<Projects.Template_Api>("template-api")
-    .WithReference(database);
+builder
+    .AddProject<Projects.Template_Api>("template-api")
+    .WithReference(database)
+    .WaitFor(database);
 
 builder.Build().Run();
